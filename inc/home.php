6f73fdaced340bb0d1f15dd93b27631320a6fe97
PH276 <pascal.huitorel@lepoles.com>
2018-03-23 00:11:29 +0100

    c2

--- inc/home.php
+++ inc/home.php
@@ -1,4 +1,15 @@
 <section id="home" class="row">
-
+    <div class="col-md-6">
+        <p>z</p>
+    </div>
+    <div class="col-md-6">
+        <p>z</p>
+    </div>
+    <div class="col-md-6">
+        <p>z</p>
+    </div>
+    <div class="col-md-6">
+        <p>z</p>
+    </div>
 
 </section>
--- index.php
+++ index.php
@@ -6,28 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- ********************** css Bootstrap ****************************** -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!-- ********************** css Bootstrap ****************************** -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/one-page.css">
+
+
     <title>Modeles</title>
 </head>
 <body>
-    <?php include('inc/header.php') ?>
+    <?php // include('inc/header.php') ?>
     <?php include('inc/menu.php') ?>
     <main class="contrainer-fluid">
         <?php include('inc/home.php') ?>
-        <?php include('inc/expertises.php') ?>
-        <?php include('inc/apply.php') ?>
-        <?php include('inc/jobs.php') ?>
-        <?php include('inc/carrer.php') ?>
-        <?php include('inc/testimonials.php') ?>
+        <?php // include('inc/expertises.php') ?>
+        <?php // include('inc/apply.php') ?>
+        <?php // include('inc/jobs.php') ?>
+        <?php // include('inc/carrer.php') ?>
+        <?php //include('inc/testimonials.php') ?>
     </main>
-    <?php include('inc/footer.php') ?>
+    <?php // include('inc/footer.php') ?>
 
     <!-- **************************** jQuery ********************************** -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
