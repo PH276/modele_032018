f1c0be37e51a9f0008bbcc90a36ddcd172e16b1e
PH276 <pascal.huitorel@lepoles.com>
2018-03-22 22:51:46 +0100

    c1

---
+++ inc/home.php
@@ -0,0 +1,4 @@
+<section id="home" class="row">
+
+
+</section>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- ********************** css Bootstrap ****************************** -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Modeles</title>
+</head>
+<body>
+    <?php include('inc/header.php') ?>
+    <?php include('inc/menu.php') ?>
+    <main class="contrainer-fluid">
+        <?php include('inc/home.php') ?>
+        <?php include('inc/expertises.php') ?>
+        <?php include('inc/apply.php') ?>
+        <?php include('inc/jobs.php') ?>
+        <?php include('inc/carrer.php') ?>
+        <?php include('inc/testimonials.php') ?>
+    </main>
+    <?php include('inc/footer.php') ?>
+
+    <!-- **************************** jQuery ********************************** -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- ********************** css Bootstrap ****************************** -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
